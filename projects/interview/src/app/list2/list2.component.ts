71f77b370e6a8c19c408df017d39b8df7a916e79
Roberto Manganelly <robmanganelly@gmail.com>
2023-04-16 14:17:28 -0400

    adding select app

--- projects/interview/src/app/list2/list2.component.ts
+++ projects/interview/src/app/list2/list2.component.ts
@@ -4,14 +4,37 @@ import { ListSkeletonComponent } from '../list-skeleton/list-skeleton.component'
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { SelectComponent } from '../select/select.component';
+import { isString } from 'lodash';
 
 @Component({
   selector: 'app-list2',
   standalone: true,
   imports: [CommonModule, ListSkeletonComponent, SelectComponent],
   templateUrl: './list2.component.html',
-  styleUrls: ['./list2.component.scss']
+  styleUrls: ['./list2.component.scss'],
 })
 export class List2Component {
+  selected = 'John-acb';
+  items: { name: string; id: string }[] = [
+    { name: 'John  Doe', id: '1123-adc1' },
+    { name: 'Mary Poppins', id: '24b9-6547' },
+    { name: 'Peter Jackson', id: '1fe1-ccd3' },
+    { name: 'Susan Saran', id: '49a1-1155' },
+    { name: 'Mike Myers', id: '1235-cd74' },
+    { name: 'Laura Paussini', id: '4fcb-1a39' },
+    { name: 'Bob Marley', id: '1322-3417' },
+    { name: 'Alice in Wonder', id: '453d-12a8' },
+  ];
 
+  valFn = function (value:{ name: string; id: string }) {
+      return `${value.name}-${value.id}`;
+    }
+
+  filterFn: Function = function (item: {name: string, id: string}, word:string) {
+      return new RegExp(word, 'i').test(`${item.name}-${item.id}`);
+  }
+
+  // dummy
+  output: string="";
+  onUpdate(v: string){this.output = v}
 }
--- projects/interview/src/app/select/select.component.ts
+++ projects/interview/src/app/select/select.component.ts
@@ -1,41 +1,75 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {CdkMenuModule} from '@angular/cdk/menu';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
-import { TableData } from '../models/TableData.model';
 import { SubSink } from 'subsink';
+import { debounceTime, tap } from 'rxjs';
+// import { TableData } from '../models/TableData.model';
 
 @Component({
   selector: 'app-select',
   standalone: true,
-  imports: [CommonModule,MatFormFieldModule,MatIconModule,MatInputModule,ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    CdkMenuModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.scss']
+  styleUrls: ['./select.component.scss'],
 })
 export class SelectComponent implements OnInit, OnDestroy {
+  protected __items: any[] = [];
+  protected searchBar = new FormControl();
 
-  searchBar = new FormControl();
+  // prop bindings
+  @Input() filterFn!: Function;
+  @Input() items!:  any[];
+  @Input() valFn!: (v: any) => string;
 
+  //two way binding
   @Input() selected!: string;
-  @Input() items!:  string[]| object[];
-  @Input() valFn = (v:any)=>v; // default dummy value function
-  @Input() filterFn = (v:any)=>v; // default dummy filter function
-  @Output() update: EventEmitter<any> = new EventEmitter<any>();
+  @Output() selectedChange = new EventEmitter<string>();
+
+  // event binding
+  @Output() update: EventEmitter<string> = new EventEmitter<string>();
 
   sub = new SubSink();
+
 
   constructor() {}
 
   ngOnInit(): void {
-    this.sub.sink = this.searchBar.valueChanges.subscribe((v:string)=>{this.update.emit(v);});
+    this.__items = [...this.items];
+    this.sub.sink = this.searchBar.valueChanges.pipe(
+      debounceTime(450),
+      tap(searchTerm=>this.__items = this.items.filter((item)=>this.filterFn(item,searchTerm))),)
+    .subscribe((v: string) => {
+      this.update.emit(v);
+    });
   }
+
+  get filteredItems(): string[] {
+    return this.__items.map(item=>this.valFn(item) );
+  }
+
   ngOnDestroy(): void {
     this.sub.unsubscribe();
   }
 
-
-
-
+  onSelectOption(v: string){
+    this.selectedChange.emit(v);
+  }
 }
