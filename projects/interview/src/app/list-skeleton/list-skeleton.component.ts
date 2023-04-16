6554c95b54813a5cf4803e5e485235687fb048bd
Roberto Manganelly <robmanganelly@gmail.com>
2023-04-15 21:31:46 -0400

    refactroe ui on entry

--- projects/interview/src/app/list-skeleton/list-skeleton.component.ts
+++ projects/interview/src/app/list-skeleton/list-skeleton.component.ts
@@ -7,23 +7,30 @@ import { RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
 import { StateService } from '../services/state.service';
 import { DataTableComponent } from '../data-table/data-table.component';
+import { MatRadioModule } from '@angular/material/radio';
 @Component({
   selector: 'app-list-skeleton',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatIconModule, RouterModule, MatButtonModule, DataTableComponent],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    RouterModule,
+    MatRadioModule,
+    MatButtonModule,
+    DataTableComponent,
+  ],
   templateUrl: './list-skeleton.component.html',
-  styleUrls: ['./list-skeleton.component.scss']
+  styleUrls: ['./list-skeleton.component.scss'],
 })
 export class ListSkeletonComponent {
-
-  @Input() title: string ="titleHERe";
-  @Input() next: string ="";
+  @Input() title: string = 'titleHERe';
+  @Input() next: string = '';
   @Input() dataSource!: Observable<any[]>; //TODO implement strong typing here.
 
   showSecondRow = false;
 
-
-  get nextRoute(){
-    return ['/list'+this.next]
+  get nextRoute() {
+    return ['/list' + this.next];
   }
 }
