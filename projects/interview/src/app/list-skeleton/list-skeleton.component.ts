1b4828a995bdd6326d6c8ff85c3606a7bb13a3c5
Roberto Manganelly <robmanganelly@gmail.com>
2023-04-17 04:57:38 -0400

    cleaning

--- projects/interview/src/app/list-skeleton/list-skeleton.component.ts
+++ projects/interview/src/app/list-skeleton/list-skeleton.component.ts
@@ -1,16 +1,14 @@
+import { ApiService } from '../services/api.service';
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterModule } from '@angular/router';
-import { Observable } from 'rxjs';
-import { StateService } from '../services/state.service';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { ApiService } from '../services/api.service';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatRadioModule } from '@angular/material/radio';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-list-skeleton',
   standalone: true,
@@ -30,15 +28,19 @@ import { ApiService } from '../services/api.service';
   providers:[{provide: ApiService, useClass: ApiService}]
 })
 export class ListSkeletonComponent {
-  @Input() title: string = 'titleHERe';
+
+  // external data source
+  @Input() title: string = '';
   @Input() next: string = '';
   @Input() viewActions = true;
 
   showSecondRow = false;
 
-  get nextRoute() {
-    return ['/list' + this.next];
-  }
+  actions = [
+    { rule: 'Add Entries', options: ['10', '20', '200'] },
+    { rule: 'Limit Entries to', options: ['2', '20', '50', '100'] },
+    { rule: 'Reset Data', options: ['Reset'] },
+  ].map(item=>({...item, call: this.onCRUD.bind(this)}))
 
   constructor(private api: ApiService) {
     this.api.executeCRUD.subscribe(([rule, option])=>{
@@ -47,11 +49,9 @@ export class ListSkeletonComponent {
     });
   }
 
-  actions = [
-    { rule: 'Add Entries', options: ['10', '20', '200'] },
-    { rule: 'Limit Entries to', options: ['2', '20', '50', '100'] },
-    { rule: 'Reset Data', options: ['Reset'] },
-  ].map(item=>({...item, call: this.onCRUD.bind(this)}))
+  get nextRoute() {
+    return ['/list' + this.next];
+  }
 
   onCRUD(rule: string, option: string){
     let _option = option === 'Reset' ? -1 : Number(option);
